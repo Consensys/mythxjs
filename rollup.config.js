0996f07cac6b5983fee677c6335d824484193b69
Mirko Garozzo <mirko@Mirkos-MacBook-Pro.local>
2019-04-16 14:58:28 +0100

    MythxJS fixed package.json + implemented rollup browser/node require

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from 'rollup-plugin-typescript2'
+import replace from 'rollup-plugin-replace'
+import pkg from './package.json'
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+        typescript({
+            typescript: require('typescript'),
+        }),
+        replace({
+            'process.browser': !!process.env.BROWSER
+        })
+    ],
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('funzionaaas')
+console.log('funzionaaassaaafs')
