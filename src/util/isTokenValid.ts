067d85d215c66224618e1529e71005328697b88c
Mirko Garozzo <mirko@Mirkos-MBP-2.homenet.telecomitalia.it>
2019-05-20 15:10:17 +0200

    MythxJS addied lint-staged as precommit

--- src/util/isTokenValid.ts
+++ src/util/isTokenValid.ts
@@ -1,25 +1,25 @@
-import * as jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken';
 
 interface jwtInterface {
-    jti: string
-    iss: string,
-    exp: number,
-    userId: string,
-    iat: number
+    jti: string;
+    iss: string;
+
+    exp: number;
+    userId: string;
+
+    iat: number;
 }
 
 export function isTokenValid(token: string): boolean {
     try {
-
         // decode token
-        const tokenDecoded: jwtInterface = jwt.decode(token)
-        const { exp } = tokenDecoded
+        const tokenDecoded: jwtInterface = jwt.decode(token);
+        const { exp } = tokenDecoded;
 
         // returns true if token is still valid
         const now = new Date();
         return now.getTime() < exp * 1000;
-
     } catch (err) {
-        throw new Error(`Error with checking if token is still valid. ${err}`)
+        throw new Error(`Error with checking if token is still valid. ${err}`);
     }
 }
