d5c8887f9f694c127af3bb2cfda2873f309f68ea
Mirko Garozzo <mirko@Mirkos-MBP-2.homenet.telecomitalia.it>
2019-05-21 12:58:53 +0200

    MythxJS cleaned test file

--- src/test/authClass.login.spec.ts
+++ src/test/authClass.login.spec.ts
@@ -1,47 +1,47 @@
-import { expect } from 'chai';
-import * as sinon from 'sinon';
+import { expect } from 'chai'
+import * as sinon from 'sinon'
 
-import { AuthService } from '../apiServices/AuthService';
-import { JwtTokensInterface } from '..';
+import { AuthService } from '../apiServices/AuthService'
+import { JwtTokensInterface } from '..'
 
 describe('loginUser', () => {
     const tokens: JwtTokensInterface = {
         access: 'access',
         refresh: 'refresh',
-    };
+    }
 
-    let loginUserStub: any;
-    let AUTH;
+    let loginUserStub: any
+    let AUTH
     beforeEach(() => {
-        AUTH = new AuthService('user', 'password');
-        loginUserStub = sinon.stub(AUTH, 'login');
-    });
+        AUTH = new AuthService('user', 'password')
+        loginUserStub = sinon.stub(AUTH, 'login')
+    })
 
     afterEach(() => {
-        loginUserStub.restore();
-    });
+        loginUserStub.restore()
+    })
 
     it('is a function', () => {
-        expect(loginUserStub).to.be.a('function');
-    });
+        expect(loginUserStub).to.be.a('function')
+    })
 
     it('should return access and refresh tokens', async () => {
-        loginUserStub.returns(tokens);
+        loginUserStub.returns(tokens)
 
-        const result = await AUTH.login();
-        expect(result).to.equal(tokens);
-    });
+        const result = await AUTH.login()
+        expect(result).to.equal(tokens)
+    })
 
     it('should fail with error ', async () => {
-        const err = 'MythxJS. Error with your request.';
+        const err = 'MythxJS. Error with your request.'
 
-        loginUserStub.rejects(new Error(err));
+        loginUserStub.rejects(new Error(err))
 
         try {
-            await AUTH.login();
-            expect.fail('login should be rejected');
+            await AUTH.login()
+            expect.fail('login should be rejected')
         } catch (err) {
-            expect(err.message).to.eql(err);
+            expect(err.message).to.eql(err)
         }
-    });
-});
+    })
+})
