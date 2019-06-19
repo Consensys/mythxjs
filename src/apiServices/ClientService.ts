9464ab311680dce62b9b482b3269b68d647744ec
Mirko Garozzo <mirkogarozzo@hotmail.it>
2019-06-19 15:08:15 +0200

    Fixed doc

--- src/apiServices/ClientService.ts
+++ src/apiServices/ClientService.ts
@@ -69,7 +69,7 @@ export class ClientService {
     /**
      *  Login to the API using metamask challenge result message.
      *  In order to get the object containing the message use `getChallenge` and handle Metamask login in the frontend.
-     * @param message string returned from `getChallenge`
+     * @param message message.value property contained in object returned from `getChallenge`.
      * @return {Promise<JwtTokensInterface>}  Returns an object containing two tokens (access+refresh) that can be saved in storage.
      */
     async loginWithMetamask(message: string): Promise<JwtTokensInterface | void> {
